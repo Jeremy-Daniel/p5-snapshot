44a9151cff19cdfdb98e37125beee316385bc885
Jeremy-Daniel <90621308+Jeremy-Daniel@users.noreply.github.com>
2024-06-28 17:16:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ canvas = createCanvas(640,480);
 canvas.position(110,250);
 webcam = createCapture(VIDEO);
 webcam.hide();
-color_name = "";
 }
 
 function draw(){
-image(webcam,0,0,640,480);
-tint(color_name);
+image(webcam,200,150,220,200);
+fill("red");
+rect(70,90,50,300);
+rect(475,90,50,300);
+circle(100,100,100);
+circle(500,100,100);
+circle(100,400,100);
+circle(500,400,100);
+rect(150,75,300,50);
+rect(150,375,300,50);
+
 }
 
 function take_snapshot(){
